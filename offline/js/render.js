73d4299cd62bcf9c050ba68cebe87e168e6bc8c6
Brighton Vino Jegarajan <brightonvino@gmail.com>
2015-09-12 04:04:37 -0700

    Tooltips and CSS

--- offline/js/render.js
+++ offline/js/render.js
@@ -1,144 +1,142 @@
-var render = function(data, container) {
-	container.classed('theateam', true);
-	var mapDiv = container.selectAll('#map');
-	if (mapDiv[0].length === 0) {
-		mapDiv = container.append('div').attr('id', 'map');
+var render = function (data, container) {
+    container.classed('theateam', true);
+    var mapDiv = container.selectAll('#map');
+    if (mapDiv[0].length === 0) {
+        mapDiv = container.append('div').attr('id', 'map');
 
-		var leaflet;
-		if ($('.sapUiBody').length !== 0) {
-			leaflet = runt.leaflet;
-		} else {
-			leaflet = L;
-		}
+        var leaflet;
+        if ($('.sapUiBody').length !== 0) {
+            leaflet = runt.leaflet;
+        } else {
+            leaflet = L;
+        }
 
-		var map = leaflet.map('map').setView([33.775, -84.40], 14);
+        var map = leaflet.map('map').setView([33.775, -84.40], 14);
 
-		leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			maxZoom: 20,
-			id: 'shobhitg.na7jk2f5',
-			accessToken: 'pk.eyJ1Ijoic2hvYmhpdGciLCJhIjoiNmI1Nzg0ZmIzMWY4OGU4MGEzYzI3ZGIxMzBhZmQ4NmUifQ.z5e8zocByNWRqW6VPfxpwg'
-		}).addTo(map);
+        leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 20,
+            id: 'shobhitg.na7jk2f5',
+            accessToken: 'pk.eyJ1Ijoic2hvYmhpdGciLCJhIjoiNmI1Nzg0ZmIzMWY4OGU4MGEzYzI3ZGIxMzBhZmQ4NmUifQ.z5e8zocByNWRqW6VPfxpwg'
+        }).addTo(map);
 
-		renderMap(data, map, container);
-	}
+        renderMap(data, map, container);
+    }
 
 };
 
 function renderMap(data, map, container) {
-	var updateMap;
-	var init = false;
+    var updateMap;
+    var init = false;
 
+    var d3Overlay = L.d3SvgOverlay(function (selection, projection) {
 
-	var d3Overlay = L.d3SvgOverlay(function(selection, projection) {
-
-		updateMap = function() {
+        updateMap = function () {
             var data = monthDimension.top(Infinity);
             //if (data.length === migrantData.length) return;
 
             console.log(data.length);
-            data = data.filter(function(d) {
+            data = data.filter(function (d) {
                 return !((d.x === "") || (d.y === ""));
             });
 
+            var feature = selection.selectAll("circle")
+                    .data(data);
+            
+            tip = d3.tip()
+                    .offset([-10, 0])
+                    .attr('class', 'd3-tip').html(function (d) {
+                return '<div class="tip">'+ d["UC2 Literal"] + '<BR><span>Crime Level: ' + d.MinOfucr + '</span></div>';
+            });
+            
+            selection.call(tip)
 
+            feature.enter().append("circle")
+                    .attr("class", "crime")
+                    .attr("r", function (d) {
+                        return (d.MinOfucr - 300) / 25 * Math.min(projection.layer._scale, 18);
+                    })
+                    .attr('cx', function (d) {
+                        return projection.latLngToLayerPoint([parseFloat(d.y), parseFloat(d.x)]).x;
+                    })
+                    .attr('cy', function (d) {
+                        return projection.latLngToLayerPoint([parseFloat(d.y), parseFloat(d.x)]).y;
+                    })
+                    .attr("fill", "#d62728")
+                    .on('mouseover', tip.show)
+                    .on('mouseout', tip.hide);
 
-			var feature = selection.selectAll("circle")
-				.data(data);
+            feature.exit().remove();
 
-			feature.enter().append("circle")
-				.style("opacity", .2)
-				.style("fill", "red")
-				.attr("r", function(d) {
-					return Math.log(d.MinOfucr) * 2 / Math.min(projection.layer._scale, 18);
-				})
-				.attr('cx', function(d) {
-					return projection.latLngToLayerPoint([parseFloat(d.y), parseFloat(d.x)]).x;
-				})
-				.attr('cy', function(d) {
-					return projection.latLngToLayerPoint([parseFloat(d.y), parseFloat(d.x)]).y;
-				});
+        };
 
-			feature.exit().remove();
+        if (init === false) {
+            init = true;
 
-			feature.attr('stroke-width', 1 / projection.layer._scale)
-				.attr("r", function(d) {
-					return Math.log(d.MinOfucr) * 2 * 1 / Math.min(projection.layer._scale, 15);
-				});
+            cfData = crossfilter(data);
 
-		};
+            var timeChartDiv = container.selectAll('#time-chart');
+            if (timeChartDiv[0].length === 0) {
+                timeChartDiv = container.append('div').attr('id', 'time-chart').classed('dc-chart', true);
 
-		if (init === false) {
-			init = true;
+                var dcjs;
+                if ($('.sapUiBody').length !== 0) {
+                    dcjs = runt.dcjs;
+                } else {
+                    dcjs = dc;
+                }
 
-			cfData = crossfilter(data);
+                var dateDimension = cfData.dimension(function (d) {
+                    return new Date(d.occur_date)
+                });
 
-			var timeChartDiv = container.selectAll('#time-chart');
-			if (timeChartDiv[0].length === 0) {
-				timeChartDiv = container.append('div').attr('id', 'time-chart').classed('dc-chart', true);
+                window.monthDimension = cfData.dimension(function (d) {
+                    return new Date(d.occur_date);
+                });
 
-				var dcjs;
-				if ($('.sapUiBody').length !== 0) {
-					dcjs = runt.dcjs;
-				} else {
-					dcjs = dc;
-				}
+                var openGroup = monthDimension.group().reduceSum(function (d) {
+                    return parseInt(d.MinOfucr);
+                });
+                closeGroup = monthDimension.group().reduce(
+                        function (p, v) {
+                            p.push(v.close);
+                            return p;
+                        },
+                        function (p, v) {
+                            p.splice(p.indexOf(v.close), 1);
+                            return p;
+                        },
+                        function () {
+                            return [];
+                        }
+                );
 
-				var dateDimension = cfData.dimension(function(d) {
-					return new Date(d.occur_date)
-				});
+                var timeChart = dc.barChart('#time-chart');
+                timeChart
+                        .width(800)
+                        .height(120)
+                        //.margins({top: 10, right: 50, bottom: 30, left: 50})
+                        .dimension(monthDimension)
+                        .group(openGroup)
+                        .x(d3.time.scale().domain([new Date("2008-01-01T00:00:00Z"), new Date("2016-09-30T00:00:00Z")]))
+                        .round(d3.time.days.round)
+                        .xUnits(d3.time.days)
+                        .elasticY(true)
+                        .on("filtered", updateMap)
+                        .filter([new Date("2011-01-01T00:00:00Z"), new Date("2012-09-30T00:00:00Z")]);
 
-				window.monthDimension = cfData.dimension(function(d) {
-					return new Date(d.occur_date);
-				});
+                dc.renderAll();
+            }
+        }
+        updateMap();
+    });
 
-				var openGroup = monthDimension.group().reduceSum(function(d) {
-					return parseInt(d.MinOfucr);
-				});
-				closeGroup = monthDimension.group().reduce(
-					function(p, v) {
-						p.push(v.close);
-						return p;
-					},
-					function(p, v) {
-						p.splice(p.indexOf(v.close), 1);
-						return p;
-					},
-					function() {
-						return [];
-					}
-				);
-
-				var timeChart = dc.barChart('#time-chart');
-				timeChart
-					.width(800)
-					.height(120)
-				//.margins({top: 10, right: 50, bottom: 30, left: 50})
-				.dimension(monthDimension)
-					.group(openGroup)
-					.x(d3.time.scale().domain([new Date("2008-01-01T00:00:00Z"), new Date("2016-09-30T00:00:00Z")]))
-					.round(d3.time.days.round)
-					.xUnits(d3.time.days)
-					.elasticY(true)
-					.on("filtered", updateMap)
-					.filter([new Date("2011-01-01T00:00:00Z"), new Date("2012-09-30T00:00:00Z")]);
-
-				dc.renderAll();
-
-
-			}
-
-		}
-
-			updateMap();
-	});
-
-	d3Overlay.addTo(map);
+    d3Overlay.addTo(map);
 }
 
-$(function() {
-	var container = d3.select('#container');
-	d3.json("./data/homepark.json", function(data) {
-		render(data, container);
-	});
+$(function () {
+    var container = d3.select('#container');
+    d3.json("./data/homepark.json", function (data) {
+        render(data, container);
+    });
 });
